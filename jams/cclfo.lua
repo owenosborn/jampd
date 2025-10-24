fa8c749c1ad531c2415341afbbd9d3565b8bccc3
Owen Osborn <owenosborn@gmail.com>
2025-10-23 23:02:23 -0400

    fix

--- jams/cclfo.lua
+++ jams/cclfo.lua
@@ -2,9 +2,9 @@ local jam = {}
 
 function jam:init(io)
     -- LFO rates in beats per cycle
-    self.rate1 = 16    -- CC 21: 16 beats per cycle
-    self.rate2 = 14    -- CC 22: 14 beats per cycle
-    self.rate3 = 12    -- CC 23: 12 beats per cycle
+    self.rate1 = 160    -- CC 21: 160 beats per cycle
+    self.rate2 = 140    -- CC 22: 140 beats per cycle
+    self.rate3 = 120    -- CC 23: 120 beats per cycle
     
     print("LFO jam initialized")
     print("CC 21: " .. self.rate1 .. " beats/cycle")
@@ -22,7 +22,7 @@ function jam:tick(io)
     local lfo2 = math.floor((math.sin(2 * math.pi * beats / self.rate2) + 1) / 2 * 127)
     local lfo3 = math.floor((math.sin(2 * math.pi * beats / self.rate3) + 1) / 2 * 127)
     
-    -- Send out CC messages 10 times per beat to avoid overwhelming MIDI
+    -- Send out CC messages x times per beat to avoid overwhelming MIDI
     if io.on(.05) then
         io.cltout(21, lfo1)
         io.cltout(22, lfo2)
