cb7eb8de6a1b3f1a389f934b27bb88d8e9f90f76
Owen Osborn <owenosborn@gmail.com>
2025-11-09 21:49:31 -0500

    utils hj

--- jam.lua
+++ jam.lua
@@ -1,14 +1,16 @@
 require ("lib/utils")
+require("lib/progression")
 
 
 function init(jam)
-    local Progression = require("lib/progression").Progression
     
     progression = Progression.new()
     --progression:parse("Fmaj7...F+7...Bbmaj7.Bo7.A-7.Abo7.G-7.C7.A-7b5.D7.G-7.C7.F6...")
-    progression:parse("G.....E-.....")
+    progression:parse("G.....E-.....G.....Em......C.....D.....G.....D.....G.....C..D..E-.....C.....D.....B7.....E-...........C...........E-...........G.....D.....G..D..")
     progression:print()    
+    chord = progression:chord()
     print("Country jam loaded")
+    count = 0
 end
 
 function ctlin(jam, n, v) 
@@ -21,19 +23,22 @@ function tick(jam)
     chord = progression:tick(jam)
     
     if jam.on(1) then
-        local bass_note = chord:note(1, 4)
+        local bass_note = chord:note(1, randi(3,4))
+        count = count+1
         jam.noteout(bass_note, 90, 0.9)
     end
 
-    -- Chord hits on strong beats, play it a little early
     if jam.on(3, 0) then  -- every 2 beats
         chord:play(jam)
     end
     
-    -- swing stuff
     if (jam.on(1) or jam.on(1, 2/3)) and p(.7) then 
         local note = chord:filter(randi(50,80))
-        jam.noteout(note, randi(60, 85), 0.4)
+        --jam.noteout(note, randi(60, 85), 0.4)
+    end
+
+    if jam.on(1/3) then 
+        
     end
 
 end
