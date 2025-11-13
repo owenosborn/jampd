3c55e099a38c97ad64f1f157100f67b5743bd4ba
Owen Osborn <owenosborn@gmail.com>
2025-11-12 22:58:14 -0500

    drum stuff

---
+++ jams/drum1.lua
@@ -0,0 +1,21 @@
+require ("lib/utils")
+require ("lib/chord")
+require("lib/progression")
+
+function init(jam)
+end
+
+function ctlin(jam, n, v) 
+    if n == 33 then 
+        jam.noteout(v//1, 60, .1)
+    end
+end
+
+function tick(jam)
+
+    if jam.on(1, -.05) then jam.noteout(40, 90, 1/4) end
+    if jam.on(choose({1, 1/2, 1/4})) and p(.5)  then jam.noteout(54, 90, 1/4) end
+    if jam.on(1/4) and p(.9)  then jam.noteout(70, choose({30, 80}), 1/8) end
+
+end
+
