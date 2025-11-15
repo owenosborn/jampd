7469626cef32fe1a70b0c6f8514cf5fda4901b51
Owen Osborn <owenosborn@gmail.com>
2025-11-15 08:19:46 -0500

    random

--- jam.lua
+++ jam.lua
@@ -1,43 +1,48 @@
-require ("lib/utils")
-require ("lib/chord")
-require("lib/progression")
 
 function init(jam)
-    cflag = false
-    cval = 0
-end
-
-function ctlin(jam, n, v)
-    if n == 33 then
-        cval = v//1
-        cflag = true
-    end
+    local Progression = require("lib/progression").Progression
+    
+    progression = Progression.new()
+    --progression:parse("Fmaj7...F+7...Bbmaj7.Bo7.A-7.Abo7.G-7.C7.A-7b5.D7.G-7.C7.F6...")
+    progression:parse("E-9...........F-7............")
+    progression:print()    
+    print("Country jam loaded - F major")
 end
 
 function tick(jam)
-    -- Hi-hats on sixteenths with accents
-    if jam.on(1/4) then 
-        local vel = jam.on(1) and 90 or 50  -- accent on downbeats
-        jam.noteout(70, vel, 1/8)
-    end
-    
-    if cflag and jam.on(1/8) then
-        cflag = false
-        jam.noteout(cval, 80, 1/8)
+    local chord = progression:tick(jam)
+    -- Alternating bass pattern (classic country style)
+    -- Root on odd beats, fifth on even beats
+    if jam.on(2) then
+        local bass_note = chord:note(1, 4)
+        jam.noteout(bass_note, 90, 0.9)
     end
 
-    -- Simple backbeat - snare on 2 and 4 with ghost notes 
-    -- use elseif so we don't play more than one note the same tick
-    if jam.on(2, 1)  then jam.noteout(54, 85, 1/4) 
+    if jam.on(2,1) then
+        local bass_note = chord:note(3, 4)
+        jam.noteout(bass_note - 12, 90, 0.9)
+    end
+
+    -- Chord hits on strong beats
+    if jam.on(2, .99) then  -- every 2 beats
+        jam.noteout(chord:note(1, 5), 70, 0.4)
+        jam.noteout(chord:note(2, 5), 70, 0.4)
+        jam.noteout(chord:note(3, 5), 70, 0.4)
+        jam.noteout(chord:note(4, 5), 70, 0.4)
+    end
     
-    -- ghost notes
-    elseif jam.on(1/3)  and p(.1)  then jam.noteout(54, 50, 1/4) 
-    elseif jam.on(1/4)  and p(.1)  then jam.noteout(54, 50, 1/4) end
-    
-    -- Kick on 1 and 3
-    if jam.on(2) then jam.noteout(40, 90, 1/4)
-    
-    -- ghost notes
-    elseif  jam.on(1/3)  and p(.1)  then jam.noteout(40, 50, 1/4) 
-    elseif  jam.on(1/4)  and p(.6)  then jam.noteout(40, 50, 1/4) end
+    -- Simple melody fills on off-beats
+    --if jam.on(1, 2/3) then  -- swing 8
+    if jam.on(0.5, 1/3) then 
+        if math.random() > 0.3 then  -- sparse, not every time
+            local note = chord:filter(math.random(50,80))
+            --jam.noteout(note, math.random(60, 85), 0.4)
+        end
+    end
+
+    if jam.on(1/3, .04) and math.random() > .1 then
+        local note = chord:filter(math.random(80,100))
+        jam.noteout(note, math.random(60, 80), 0.5)
+    end
 end
+
---
+++ jams/drum3-triplets.lua
@@ -0,0 +1,44 @@
+require ("lib/utils")
+require ("lib/chord")
+require("lib/progression")
+
+function init(jam)
+    cflag = false
+    cval = 0
+end
+
+function ctlin(jam, n, v)
+    if n == 33 then
+        cval = v//1
+        cflag = true
+    end
+end
+
+function tick(jam)
+    -- Hi-hats on sixteenths with accents
+    if jam.on(1/3) then 
+        --local vel = jam.on(1) and 90 or 50  -- accent on downbeats
+        local vel = choose({50, 80})
+        jam.noteout(70, vel, 1/8)
+    end
+    
+    if cflag and jam.on(1/8) then
+        cflag = false
+        jam.noteout(cval, 80, 1/8)
+    end
+
+    -- Simple backbeat - snare on 2 and 4 with ghost notes 
+    -- use elseif so we don't play more than one note the same tick
+    if jam.on(2, 1.05)  then jam.noteout(54, 85, 1/4) 
+    
+    -- ghost notes
+    elseif jam.on(1/3)  and p(.1)  then jam.noteout(54, 50, 1/4) 
+    elseif jam.on(1/6)  and p(.1)  then jam.noteout(54, 50, 1/4) end
+    
+    -- Kick on 1 and 3
+    if jam.on(2) then jam.noteout(40, 90, 1/4)
+    
+    -- ghost notes
+    elseif  jam.on(1/3, .04)  and p(.1)  then jam.noteout(40, 50, 1/4) 
+    elseif  jam.on(1/6, .02)  and p(.6)  then jam.noteout(40, 50, 1/4) end
+end
